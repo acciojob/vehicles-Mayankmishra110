a2373e712b7c87bc6fc2b3958c476df962d3fcbc
Mayankmishra110 <kmmay15@gmail.com>
2023-01-21 15:10:25 +0530

    Solution of contest

---
+++ src/main/java/com/driver/boat1.java
@@ -0,0 +1,12 @@
+package com.driver;
+
+public class Boat1 implements WaterVehicle {
+    private String name;
+    private int capacity;
+    public String getVehicleName(){
+        return name;
+    }
+    public int getVehicleCapacity(){
+        return capacity;
+    }
+}
